06f842f84861c1e5298ffd05050d9df21d387636
LF Yang <51221650+coffeeling12@users.noreply.github.com>
2020-10-25 20:45:23 -0400

    Update toxic.py

--- code/toxic.py
+++ code/toxic.py
@@ -10,14 +10,14 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-# import seaborn as sns
 from collections import defaultdict, Counter
-from sklearn.decomposition import TruncatedSVD
-from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+# from sklearn.decomposition import TruncatedSVD
+# from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import cross_val_score, cross_val_predict
-from scipy.sparse import hstack
-from scipy.special import logit, expit
+# from sklearn.model_selection import cross_val_score, cross_val_predict
+# from scipy.sparse import hstack
+# from scipy.special import logit, expit
 
 os.getcwd()
 train = pd.read_csv('../data/train.csv')
@@ -41,6 +41,13 @@ class_names = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity
 train_df = train['comment_text']
 test_df = test['comment_text']
 y_train = train[class_names]
+test_id = test['id']
+
+
+train_x, validate_x, train_y, validate_y = train_test_split(
+                        train_df, y_train,
+                        test_size=0.3,
+                        random_state=0)
 
 import re
 import nltk
@@ -65,16 +72,13 @@ train_update.values[:10]
 token_train = [train_comment.split() for train_comment in train_update]
 train_data = token_train
 
+train_x_update = preprocessing(train_x)
+validate_x_update = preprocessing(validate_x)
+
 test_update = preprocessing(test_df)
 test_data = [test_comment.split() for test_comment in test_update]
 
-token_counter = Counter()
-for tokens in token_train:
-    token_counter.update(tokens)
-token_cnt_list = sorted(token_counter.items(), key = lambda x: x[1], reverse = True)
-    
-for token, cnt in token_cnt_list[:9]:
-    print(f'token={token}\tcount={cnt}')
+
 
 # Lemmatize words using the WordNetLemmatizer. You can ignore any word that is not longer than one character.
 wnl = nltk.stem.WordNetLemmatizer()
@@ -85,13 +89,18 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 # use tf-idf
 tfidf = TfidfVectorizer(
-           ngram_range=(1, 3),
+           ngram_range=(1, 3),  # 二元文法模型
            use_idf=1,
            smooth_idf=1,
-           stop_words = 'english') 
+           stop_words = 'english') # 去掉英文停用词
 
 data_train_count = tfidf.fit_transform(train_update)
+
+data_train_x_count = tfidf.fit_transform(train_x_update)
+data_validate_x_count = tfidf.fit_transform(validate_x_update)
+
 data_test_count  = tfidf.transform(test_update)
+
 
 # text_encoder = Pipeline([
 #     ('tfidf_vec', TfidfVectorizer(stop_words="english", min_df = 5, max_df = 0.9)),
@@ -101,21 +110,39 @@ data_test_count  = tfidf.transform(test_update)
 
 # baseline - naive bayes
 from sklearn.naive_bayes import MultinomialNB 
-
 clf = MultinomialNB()
-y = y_train.to_numpy()
-clf.fit(data_train_count, y)
-pred = clf.predict(data_test_count)
-print (pred)
 
+def predict_test(data_train_count, y_train, test, data_test_count):
+    y = y_train.to_numpy()
+    row = test.shape[0]
+    var_len = y.shape[1]
+    pred = np.zeros([row, var_len])
+    
+    for i in range(var_len):
+        y1 = y[:,i]
+        clf.fit(data_train_count, y1)
+        pred[:,i] = clf.predict(data_test_count)
+    return pred
+
+pred_tot = predict_test(data_train_count, y_train, test, data_test_count)
+pred_train = predict_test(data_train_x_count, train_y, test, data_test_count)
+pred_val = predict_test(data_validate_x_count, validate_y, test, data_test_count)
+
+df_out =pd.DataFrame(data = pred_tot, columns=class_names)
+df_out.insert(0, "id", test['id'])
+df_out.to_csv('submission.csv',index = False, header=True)
 
 # baseline - logistic regression
 model = LogisticRegression(penalty='l2', tol=0.0001, C=1.0, fit_intercept=True, solver='lbfgs', max_iter=30, 
-                           multi_class='multinomial', verbose=1, n_jobs=6)
+                           multi_class='auto', verbose=1, n_jobs=6)
 
-model.fit(data_train_count, y_train)
-y_hat_train = model.predict(X_train)
-y_hat_val = model.predict(X_val)
+model = LogisticRegression(penalty='l2', tol=0.0001, C=1.0, fit_intercept=True, solver='lbfgs', max_iter=30, 
+                           multi_class='auto', verbose=1)
+
+y1 = y_train[:,0]
+model.fit(data_train_count, y1)
+y_hat_train = model.predict(data_train_count)
+y_hat_val = model.predict(data_test_count)
 
 # Evaluation
 from sklearn.metrics import accuracy_score
